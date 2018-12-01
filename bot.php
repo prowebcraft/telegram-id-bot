da2b5fd23fcbefeda064ec275e7b491bca58b76d
Andrey Mistulov <a.mistulov@aristos.pw>
2018-12-01 11:32:10 +0300

    Support both console and webhook mode

--- bot.php
+++ bot.php
@@ -6,8 +6,11 @@ require_once "IdBot.php";
 $config = [];
 $bot = new IdBot('Identy Bot', 'Simple bot showing id of users or groups', 'prowebcraft', 'prowebcraft@gmail.com', []);
 
-//Run in webhook mode
-$bot->webhook();
 
-//Run in daemon mode
-//$bot->start();
+if ($bot->isConsoleMode()) {
+    //Run in daemon mode
+    $bot->start();
+} else {
+    //Run in webhook mode
+    $bot->webhook();
+}
