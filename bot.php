a25a5a31140715fd4ee3931c3092669d856ef82f
Andrey Mistulov <a.mistulov@aristos.pw>
2019-08-01 15:11:19 +0300

    Improve startup script

--- bot.php
+++ bot.php
@@ -6,10 +6,13 @@ require_once "IdBot.php";
 $config = [];
 $bot = new IdBot('Identy Bot', 'Simple bot showing id of users or groups', 'prowebcraft', 'prowebcraft@gmail.com', []);
 
-
 if ($bot->isConsoleMode()) {
     //Run in daemon mode
-    $bot->start();
+    if (isset($argv[1]) && $argv[1] == 'webhook') {
+        $bot->webhook();
+    } else {
+        $bot->start();
+    }
 } else {
     //Run in webhook mode
     $bot->webhook();
